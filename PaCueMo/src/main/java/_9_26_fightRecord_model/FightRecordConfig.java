f8e7e6dec6f6fbaa6f267f4aafe1a3c8ec7706b1
徐子然 <ggininder@gmail.com>
2016-09-01 20:05:31 +0800

    新增fightRecordModel

---
+++ PaCueMo/src/main/java/_9_26_fightRecord_model/FightRecordConfig.java
@@ -0,0 +1,15 @@
+package _9_26_fightRecord_model;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+
+import _00_config.RootConfig;
+
+@Import(RootConfig.class)
+@Configuration
+@ComponentScan("_9_26_fightRecord_model")
+public class FightRecordConfig
+{
+
+}
